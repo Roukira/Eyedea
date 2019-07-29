93367af64b324e2f63a60440e3104c7ce75ec770
Roukira <yyy.yousfi@gmail.com>
2019-07-29 17:51:07 +0200

    Text on plus button

---
+++ postIdea.js
@@ -0,0 +1,11 @@
+function showText(){
+    document.getElementById("newIdeaButtonLabel").style.display = "inline-block";
+    document.getElementById("newIdeaButtonLabel").style.marginLeft = "70px";
+    document.getElementById("newIdeaButtonLabel").style.marginBottom = "70px";
+    document.getElementById("newIdeaButtonLabel").style.height = "12px";
+    document.getElementById("newIdeaButtonLabel").style.cssFloat = "left";
+}
+
+function hideText(){
+    document.getElementById("newIdeaButtonLabel").style.display = "none";
+}
